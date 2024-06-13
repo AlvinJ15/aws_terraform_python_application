1e9f6bdad07dda72cf29825da8c8fd192a73ca90
Alvin Chunga <alvinchma@gmail.com>
2024-06-12 22:02:00 -0500

    Update document expiry lambda and event rule names
    Updated the 'lambda_documents_expired.py' to also send an email with the list of expired documents. Additionally, the AWS CloudWatch event rule names in the Terraform files have been modified to concatenate the 'project_name' and 'env' variables for more specificity. The changes will ensure better tracking of expired documents and provide more readable event names.

--- src/api_services/lambdas_functions/lambda_documents_expired.py
+++ src/api_services/lambdas_functions/lambda_documents_expired.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+from api_services.lambdas_functions.lambda_documents_expiring_soon import build_employee_row, build_employee_header
+from api_services.utils.csv_utils import create_csv_in_memory
 from api_services.utils.database_utils import DataBase
 from api_services.utils.decorators_utils import handle_exceptions, set_stage
+from api_services.utils.ses_utils import SES
 from data_models.model_employee import Employee
 from data_models.model_employee_document import EmployeeDocument
 
@@ -14,12 +17,14 @@ def expired_handler(event, context, stage):
         employee_documents = db.query(EmployeeDocument).filter(
             (EmployeeDocument.expiry_date < current_datetime)
         ).order_by(EmployeeDocument.employee_id)
+        employees_dict = {}
 
         for document in employee_documents:
             document.status = 'Expired'
         employee_ids = [doc.employee_id for doc in employee_documents]
         employees = db.query(Employee).filter(Employee.employee_id.in_(employee_ids)).all()
         for employee in employees:
+            employees_dict[employee.employee_id] = employee
             if employee.compliance_tags:
                 if 'DOCUMENTS_EXPIRY_SOON' in employee.compliance_tags:
                     employee.compliance_tags = employee.compliance_tags.replace('DOCUMENTS_EXPIRY_SOON', '')
@@ -27,7 +32,18 @@ def expired_handler(event, context, stage):
                     employee.compliance_tags += ',DOCUMENTS_EXPIRED'
             else:
                 employee.compliance_tags = 'DOCUMENTS_EXPIRED'
+
+        rows = [
+            build_employee_row(document, employees_dict[document.employee_id]) for document in employee_documents
+        ]
+        body = '<BR>Expired Employee Documents Attached<BR><BR>'
+        csv_file = create_csv_in_memory(build_employee_header(), rows)
+        SES.send_email_credentially(
+            'credentialing@tollanis.com', 'EXPIRED DOCUMENTS',
+            body, csv_file, 'expired_documents.csv'
+        )
         db.commit()
+
         return {
             'statusCode': 201,
             'body': 'Successfully updated documents expired'
